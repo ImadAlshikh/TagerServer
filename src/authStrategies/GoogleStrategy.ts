d2e40cb620f4ffa76e063bb119da6eb1aed9d9ba
ImadAlshikh <iimadgameover@gmail.com>
2026-01-05 11:53:35 +0300

    edits for deploy

--- src/authStrategies/GoogleStrategy.ts
+++ src/authStrategies/GoogleStrategy.ts
@@ -6,7 +6,7 @@ const googleStrategy: GoogleStrategy = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: "http://localhost:3001/auth/google/callback",
+    callbackURL: "process.env.BACKEND_URL/auth/google/callback",
     scope: ["profile", "email"],
   },
   async (accessToken, refreshToken, profile, done) => {
