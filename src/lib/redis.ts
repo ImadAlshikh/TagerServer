608f4db58eea2364fdc9f0af50e6cc7befafb4e8
ImadAlshikh <iimadgameover@gmail.com>
2026-01-31 22:55:46 +0300

    fix redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,6 @@
-import Redis from "ioredis";
+import { Redis } from "ioredis";
+import env from "./env.js";
 
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+const redis = new Redis(env.redis.url || "redis://localhost:6379");
 
 export default redis;
